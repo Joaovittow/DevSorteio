93997df3bb16fbcfd153b5a8ecd47c08490e5f0a
JoaoVitor171221 <joaovitorseixasn@gmail.com>
2023-07-25 21:29:31 -0300

    update scripts

--- scripts.js
+++ scripts.js
@@ -3,8 +3,15 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    alert(result)
     
+
+    
+ 
+    if (max > min) {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(result)
+
+    } else {
+        alert('O valor máximo tem que ser MAIOR que o valor mínimo!')
+    }
 }
